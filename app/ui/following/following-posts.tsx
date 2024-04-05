a75cdfd231ab91857678d901885cfb68458d92c2
Sh1nG0725 <161466000+Sh1nG0725@users.noreply.github.com>
2024-04-05 10:55:47 +0900

    Update following-posts.tsx
    fetchFollowingPostsをサーバーアクションに変更

--- app/ui/following/following-posts.tsx
+++ app/ui/following/following-posts.tsx
@@ -1,7 +1,12 @@
 import { fetchFollowingPosts } from '@/app/lib/data';
 import { Posts } from './posts-following';
 
+async function fetch() {
+  "use server"
+  return await fetchFollowingPosts(0);
+}
+
 export default async function FollowingPosts() { 
-  const followingPosts = await fetchFollowingPosts(0);
+  const followingPosts = await fetch();
   return <Posts initialItems={followingPosts} fetchFollowingPosts={fetchFollowingPosts} />;
 }
